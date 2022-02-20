bb33c56d334721d5dd01ca62c0422806f7c34594
Danilo <96477921+danilojezernik@users.noreply.github.com>
2022-02-20 12:18:13 +0100

    Add files via upload

--- todolist.js
+++ todolist.js
@@ -1,4 +1,4 @@
-let dodajToDo = document.getElementById("dodajToDo");
+let dodajDatum = document.getElementById("dodajDatum");
 let toDoVsebina = document.getElementById("toDoVsebina");
 let dodajPolje = document.getElementById("dodajPolje");
 let ustvarjeniElementi = [];
@@ -6,13 +6,27 @@ let ustvarjeniElementi = [];
 
 function add() {
     let text = dodajPolje.value;
+    let datum = dodajDatum.value;
     let element = document.createElement("div");
     element.innerHTML = `
-<div class="d-flex bd-highlight p-2 mb-3 shadow border border-1 rounded-3">
-  <div class="p-2 flex-grow-1 bd-highlight"><h2 id="precrtano${ustvarjeniElementi.length}">${text}</h2></div>
-  <div class="p-2 bd-highlight"><button class="btn btn-danger" onclick="odstraniElement(${ustvarjeniElementi.length})">Odstrani</button></div>
-  <div class="p-2 bd-highlight"><button class="btn btn-success" onclick="opravilElement(${ustvarjeniElementi.length})">Opravljeno</button></div>
-</div>`;
+
+<div class="d-flex bd-highlight p-3 mt-4 shadow border border-1 rounded-3">
+
+  <div class="p-2 flex-grow-1 bd-highlight"><h2 id="precrtano${ustvarjeniElementi.length}">* ${text}</h2></div>
+  <div class="p-2 bd-highlight">
+  <button class="btn btn-danger" onclick="odstraniElement(${ustvarjeniElementi.length})">Odstrani</button>
+  </div>
+  <div class="p-2 bd-highlight">
+  <button class="btn btn-success" onclick="opravilElement(${ustvarjeniElementi.length})">Opravljeno</button>
+  </div>
+</div>
+<div class="d-flex flex-row-reverse bd-highlight">
+  <div class="pt-3 bd-highlight border-bottom border-danger">
+  <p id="dodajDatum">Dodano: ${datum}</p>
+</div>
+</div>
+
+`;
     toDoVsebina.appendChild(element);
     ustvarjeniElementi.push(element);
     reset();
@@ -36,4 +50,5 @@ function clearAll() {
 
 function reset() {
     document.getElementById("dodajPolje").value = "";
+    document.getElementById("dodajDatum").value = "";
 }
